47e84a3fb6afadb1b9623498cc4049f102d63a05
rodrigorar <rodrigo.ra.rosa@gmail.com>
2022-12-10 18:25:44 +0000

    tasks-desktop: base electron structure

---
+++ tasks-desktop/src/main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 500,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'home/home.html'));
+};
+
+app.on('ready', () => {
+  createWindow()
+
+  Menu.setApplicationMenu(null);
+});
