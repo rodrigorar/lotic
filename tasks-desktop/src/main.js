b394627e3c23d69009ad15879846439b698d967f
rodrigorar <rodrigo.ra.rosa@gmail.com>
2022-12-11 16:44:23 +0000

    Tasks-Desktop: Basic UI design

--- tasks-desktop/src/main.js
+++ tasks-desktop/src/main.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
-    width: 500,
-    height: 600,
+    width: 400,
+    height: 500,
     resizable: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
