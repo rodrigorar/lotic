33286d4bfb9d4212fcb8d26c95f273626a119dbb
rodrigorar <rodrigo.ra.rosa@gmail.com>
2022-12-11 22:06:58 +0000

    Tasks-Desktop: add axios client to the app

--- tasks-desktop/src/main.js
+++ tasks-desktop/src/main.js
@@ -3,11 +3,19 @@ const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 const { OSMask } = require('./modules/shared/os-mask');
 const { isDev } = require('./modules/shared/utils');
+const { HttpClient } = require('./modules/shared/http');
 
 // Prepare local data directories
 OSMask.prepareDataDirIfNecessary(isDev);
 
 runSchemaMigrations();
+
+HttpClient.get('/persons/1')
+  .then(result => {
+    if (result) {
+      console.log(result.data);
+    }
+  });
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
---
+++ tasks-desktop/src/modules/shared/http.js
@@ -0,0 +1,59 @@
+const axios = require('axios');
+
+const client = axios.create({
+    baseURL: 'http://localhost:5000',
+    timeout: 5000
+});
+
+async function doCall(httpCall) {
+    try {
+        return await httpCall();
+    } catch (error) {
+        console.log(error.data);
+    }
+}
+
+const get = async (url) => {
+    return await doCall(() => 
+        client.get(url, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }));
+};
+
+const post = async (url, data) => {
+    return await doCall(() =>
+        client.post(url, data, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }));
+};
+
+const put = async (url, data) => {
+    return await doCall(() => 
+        client.put(url, data, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }));
+};
+
+const del = async (url) => {
+    return await doCall(() =>
+        client.delete(url, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }));
+};
+
+module.exports.HttpClient = {
+    get,
+    post,
+    put,
+    del
+}
