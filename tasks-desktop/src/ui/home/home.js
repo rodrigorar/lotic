e2397d3573efeee150ae7e11b59ad57e08db6b0f
rodrigorar <rodrigo.ra.rosa@gmail.com>
2022-12-14 19:31:24 +0000

    Tasks-Desktop: Text input/edit for tasks in list

--- tasks-desktop/src/ui/home/home.js
+++ tasks-desktop/src/ui/home/home.js
@@ -5,8 +5,14 @@ function createTaskDOM(id, contents) {
     const containerDiv = document.createElement('div');
     containerDiv.id = 'task-' + id;
 
+    const text = document.createElement('input');
+    text.classList.add('transparent-text-input');
+    text.id = 'task-' + id + '-text';
+    text.type = 'text';
+    text.placeholder = 'Tasks title'
+
     const span = document.createElement('span');
-    span.innerText = contents;
+    span.appendChild(text);
 
     const input = document.createElement('input');
     input.type = 'checkbox';
