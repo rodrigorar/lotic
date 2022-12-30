c976b0352549810b9a42f736e580074657900a2b
rodrigorar <rodrigo.ra.rosa@gmail.com>
2022-12-30 21:43:58 +0000

    Tasks-Backend: Add business rule providers

--- tasks-backend/src/domain/tasks/businessrules.py
+++ tasks-backend/src/domain/tasks/businessrules.py
@@ -25,3 +25,22 @@ class ListTasksForUser(BaseBusinessRule):
 
     def execute(self, port):
         raise NotImplemented("ListTasksForUser#execute is not implemented.")
+
+
+class TasksBusinessRulesProvider:
+
+    @staticmethod
+    def create_tasks(unit_of_work) -> CreateTasks:
+        raise NotImplemented("TasksBusinessRulesPovider#create_tasks is not implemented.")
+
+    @staticmethod
+    def update_tasks(unit_of_work) -> UpdateTasks:
+        raise NotImplemented("TasksBusinessRulesProvider#update_tasks is not implemented.")
+
+    @staticmethod
+    def delete_tasks(unit_of_work) -> DeleteTasks:
+        raise NotImplemented("TasksBusinessRulesProvider#delete_tasks is not implemented.")
+
+    @staticmethod
+    def list_tasks_for_user(unit_of_work) -> ListTasksForUser:
+        raise NotImplemented("TasksBusinessRulesProvider#list_tasks_for_user is not implemented.")
--- tasks-backend/src/domain/users/businessrules.py
+++ tasks-backend/src/domain/users/businessrules.py
@@ -11,3 +11,14 @@ class GetUser(BaseBusinessRule):
 
     def execute(self, port):
         raise NotImplemented("GetUser#execute is not implemented.")
+
+
+class UserBusinessRulesProvider:
+
+    @staticmethod
+    def create_user(unit_of_work) -> CreateUser:
+        raise NotImplemented("UserBusinessRulesProvider#created_user is not implemented.")
+
+    @staticmethod
+    def get_user(unit_of_work) -> GetUser:
+        raise NotImplemented("#UserBusinessRulesProvider#get_user is not implemented.")
