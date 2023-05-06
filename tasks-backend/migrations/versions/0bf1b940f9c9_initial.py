4ae9ddecbbc831fe27d5ad782c30664812defaba
rodrigorar <rodrigo.ra.rosa@gmail.com>
2023-05-06 17:51:01 +0100

    Tasks-Backend: Initial configuration of Alembic

---
+++ tasks-backend/migrations/versions/0bf1b940f9c9_initial.py
@@ -0,0 +1,24 @@
+"""initial
+
+Revision ID: 0bf1b940f9c9
+Revises: 
+Create Date: 2023-05-06 17:30:11.819721
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0bf1b940f9c9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
