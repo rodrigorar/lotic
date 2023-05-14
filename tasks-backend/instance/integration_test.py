3bbe5d1d27e2a6bdbb2612e1467b9b9aacad0801
rodrigorar <rodrigo.ra.rosa@gmail.com>
2023-05-14 16:11:23 +0100

    Tasks-Backend: Remove stdout logs from integration tests

--- tasks-backend/instance/integration_test.py
+++ tasks-backend/instance/integration_test.py
@@ -1,7 +1,7 @@
 import logging
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///integration_tests.sqlite"
-LOGGING_CONFIG = "instance/logging.conf"
+LOGGING_CONFIG = "instance/integration_logging.conf"
 LOGGING_LEVEL = logging.INFO
 SECRET_KEY = "ZDk0OWQ5ZDYtNjQxYy00YWE3LTlhZTktZTU0Njg0NzhjZjRmCg=="
 ACCESS_TOKEN_TTL_HOURS = 1
